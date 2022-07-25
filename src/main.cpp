9525de21d91ad902e058453c7cc69e47d0865f50
MehtabAzamkhan <kmehtab285@gmail.comm>
2022-07-25 19:13:50 +0500

    added esp32 Code with firebase configure

--- src/main.cpp
+++ src/main.cpp
@@ -1,115 +1,49 @@
-/*
- * Firebase ESP32
- * https://www.electroniclinic.com/
- */
-
 #include <WiFi.h>
 #include <FirebaseESP32.h>
-
-#include <Arduino.h>
+ 
 #include <LiquidCrystal_I2C.h>
-
-
-#include "EmonLib.h"
-#define vCalibration 83.3
-#define currCalibration 0.50
-
-
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <LiquidCrystal.h>
+#include "EmonLib.h"    
+ 
 #define FIREBASE_HOST "https://hello-52c2a-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "PlhqAYWRrLfO1HqtO5GzR9TsNGDXt1QUlEWkbTvA"
 #define WIFI_SSID "Shoaib"
 #define WIFI_PASSWORD "A0123456789"
+ 
+ 
+//Define FirebaseESP32 data object
+FirebaseData firebaseData;
+FirebaseJson json;
+//int Vresistor = A0; 
+//int Vrdata = 0; 
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
-EnergyMonitor emon;
-
+EnergyMonitor emon1;             // Create an instance
 float kWh = 0;
 unsigned long lastmillis = millis();
 
 
-
-//Define FirebaseESP32 data object
-FirebaseData firebaseData;
-FirebaseJson json;
-
-
-int   Vo = 32; 
-int Vodata = 0;
-
-int Cu = 33;
-int Cudata = 0;
-
-int KWh;
-int kwdata = 0;
-
-int Po;
-int Pdata = 0;
-
-
-
-void file()
-{
-  emon.calcVI(20, 2000);
-  kWh = kWh + emon.apparentPower * (millis() - lastmillis) / 3600000000.0;
-  yield();
-  Serial.print("Vrms: ");
-  Serial.print(emon.Vrms, 2);
-  Serial.print("V");
+int Irms = 0;
+int KWh = 0;
+int supplyVoltage = 0;
+int powerFActor = 0;
+int apparentPower = 0;
+int realPower = 0;
  
-  Serial.print("\tIrms: ");
-  Serial.print(emon.Irms, 4);
-  Serial.print("A");
- 
-  Serial.print("\tPower: ");
-  Serial.print(emon.apparentPower, 4);
-  Serial.print("W");
- 
-  Serial.print("\tkWh: ");
-  Serial.print(kWh, 5);
-  Serial.println("kWh");
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Vrms:");
-  lcd.print(emon.Vrms, 2);
-  lcd.print("V");
-  lcd.setCursor(0, 1);
-  lcd.print("Irms:");
-  lcd.print(emon.Irms, 4);
-  lcd.print("A");
-  delay(2500);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Power:");
-  lcd.print(emon.apparentPower, 4);
-  lcd.print("W");
-  lcd.setCursor(0, 1);
-  lcd.print("kWh:");
-  lcd.print(kWh, 4);
-  lcd.print("W");
-  delay(2500);
- 
-  lastmillis = millis();
- 
- 
-}
-
-
-
-
-
-
 void setup()
 {
-  lcd.init();
-  lcd. backlight ();
-  Serial.begin(115200);
-  lcd.begin(16, 2);
  
+  Serial.begin(115200);
+  lcd.init();
+  // turn on LCD backlight                      
+  lcd.backlight();
+  
+  emon1.voltage(35, 220, 1.7);  // Voltage: input pin, calibration, phase_shift
+  emon1.current(34, 0.5);   
+ 
+ //pinMode(Vresistor, INPUT);
 
-
-
+ 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED)
@@ -121,78 +55,139 @@ void setup()
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
-
+ 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
-
+ 
   //Set database read timeout to 1 minute (max 15 minutes)
   Firebase.setReadTimeout(firebaseData, 1000 * 60);
   //tiny, small, medium, large and unlimited.
   //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
   Firebase.setwriteSizeLimit(firebaseData, "tiny");
-
+ 
   /*
   This option allows get and delete functions (PUT and DELETE HTTP requests) works for device connected behind the
   Firewall that allows only GET and POST requests.
   
   Firebase.enableClassicRequest(firebaseData, true);
   */
-
-  String path = "/data";
+ 
+  //String path = "/data";
   
-
+ 
   Serial.println("------------------------------------");
   Serial.println("Connected...");
-
-
-
-  emon.voltage(35, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
-  emon.current(34, currCalibration); // Current: input pin, calibration.
- 
-  
-  lcd.setCursor(3, 0);
-  lcd.print("IoT Energy");
-  lcd.setCursor(5, 1);
-  lcd.print("Meter");
-  delay(3000);
-  lcd.clear();
   
 }
-
-
+ 
 void loop()
 {
-
- Vodata = analogRead(emon.Vrms);
- int Vdata = map(Vodata,0,4095,0,1000);
- Serial.println(Vdata); 
-
- Cudata = analogRead(emon.Irms);
- int Cdata = map(Cudata,0,4095,0,1000);
- Serial.println(Cdata);
-
- Po = analogRead(emon.apparentPower);
- int Pdata = map(Po,0,4095,0,1000); 
- Serial.println(Pdata); 
-
-  KWh = analogRead(kWh);
- int Kdata = map(KWh,0,4095,0,1000); 
- Serial.println(Kdata); 
-
-  json.set("/Data", Cdata);
-  Firebase.updateNode(firebaseData,"Current",json);
-
-  json.set("/Data", Vdata);
-  Firebase.updateNode(firebaseData,"Voltage",json);
-
-  json.set("/Data", Pdata);
-  Firebase.updateNode(firebaseData,"Power",json);
- 
-  json.set("/Data", Kdata);
-  Firebase.updateNode(firebaseData,"kWh",json);
   
+emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
+  kWh = kWh + emon1.apparentPower * (millis() - lastmillis) / 3600000000.0;
+           
 
-} 
+  float realPower       = emon1.realPower;  //extract Real Power into variable
+  Serial.print("\tReal Power: ");
+  Serial.println(realPower);
+  lcd.setCursor(0, 0);
+  lcd.print("R.Power:");
+  lcd.print(realPower);
+  lcd.print("W");
+  int RPdata = map(realPower,0,4095,0,1000);
+  Serial.println(RPdata); 
+  delay(100); 
+  json.set("/data", RPdata);
+  Firebase.updateNode(firebaseData,"/Real Power",json);
+
+
+  
+  float apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
+  Serial.print("\tApparent Power: ");
+  Serial.println(apparentPower);
+  lcd.setCursor(0, 1);
+  lcd.print("A.Power:");
+  lcd.print(apparentPower);
+  lcd.print("W");
+  int APdata = map(apparentPower,0,4095,0,1000);
+  Serial.println(APdata); 
+  delay(100); 
+  json.set("/data1", APdata);
+  Firebase.updateNode(firebaseData,"/Apparent Power",json);
+  delay(2000);
+
+
+  
+   
+  lcd.clear();
+  float powerFActor = emon1.powerFactor;      //extract Power Factor into Variable
+  Serial.print("\tPower Factor: ");
+  Serial.println(powerFActor);
+  lcd.setCursor(0, 0);
+  lcd.print("P.Factor:");
+  lcd.print(powerFActor);
+  lcd.print("W");
+  int PFdata = map(powerFActor,0,4095,0,1000);
+  Serial.println(PFdata); 
+  delay(100); 
+  json.set("/data2", PFdata);
+  Firebase.updateNode(firebaseData,"/Power Factor",json);
+
+
+   
+  float supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
+  Serial.print("\tVoltage: ");
+  Serial.println(supplyVoltage); 
+  lcd.setCursor(0, 1);
+  lcd.print("Voltage:");
+  lcd.print(supplyVoltage);
+  lcd.print("V");
+  int Vdata = map(supplyVoltage,0,4095,0,1000);
+  Serial.println(Vdata); 
+  delay(100); 
+  json.set("/data3", Vdata);
+  Firebase.updateNode(firebaseData,"/Voltage",json);
+  delay(3000);
 
 
 
+
+
+
+  lcd.clear();
+  float Irms = emon1.Irms;             //extract Irms into Variable
+  Serial.print("\tIrms: ");
+  Serial.println(Irms);
+  lcd.setCursor(0, 0);
+  lcd.print("Irms:");
+  lcd.print(Irms);
+  lcd.print("A");  
+  int Idata = map(Irms,0,4095,0,1000);
+  Serial.println(Idata); 
+  delay(100); 
+  json.set("/data4", Idata);
+  Firebase.updateNode(firebaseData,"/Current",json);
+
+
+  
+  Serial.print("\tKwh: ");
+  Serial.println(kWh);
+  lastmillis = millis();
+  lcd.setCursor(0, 1);
+  lcd.print("Kwh:");
+  lcd.print(kWh);
+  lcd.print("W");
+  int Kdata = map(kWh,0,4095,0,1000);
+  Serial.println(Kdata); 
+  delay(100); 
+  json.set("/data5", Kdata);
+  Firebase.updateNode(firebaseData,"/KWh",json);
+  delay(1000);
+
+ 
+}
+
+   
+
+
+  
