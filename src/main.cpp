4ae483a686a772bc2d94a8c0c4e08ede58460330
MehtabAzamkhan <kmehtab285@gmail.comm>
2022-08-24 03:32:51 +0500

    olid Added

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1351.h>
+#include <SPI.h>
 #include <WiFi.h>
 #include <FirebaseESP32.h>
- 
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-#include <LiquidCrystal.h>
 #include "EmonLib.h"    
- 
+
+// Screen dimensions
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
+
+// You can use any (4 or) 5 pins 
+#define SCLK_PIN 18
+#define MOSI_PIN 23
+#define DC_PIN   16
+#define CS_PIN   17
+#define RST_PIN  5
+
+// Color definitions
+#define  BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
 #define FIREBASE_HOST "https://final-98305-default-rtdb.firebaseio.com/"
 #define FIREBASE_AUTH "Hb68fePoOC5RhGPbFM8Sg9NhjTpPCs3dV55Rfk00"
 #define WIFI_SSID "robeel047"
 #define WIFI_PASSWORD "33445566"
- 
- 
+
+
 //Define FirebaseESP32 data object
 FirebaseData firebaseData;
 FirebaseJson json;
@@ -23,20 +43,30 @@ float kWh = 0;
 unsigned long lastmillis = millis();
 
 
-int Irms = 0;
-int KWh = 0;
-int supplyVoltage = 0;
-int powerFActor = 0;
-int apparentPower = 0;
-int realPower = 0;
- 
+float Irms = 0;
+float KWh = 0;
+float supplyVoltage = 0;
+float powerFActor = 0;
+float apparentPower = 0;
+float realPower = 0;
+
+Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);  
+float p = 3.1415926;
+
+
+
 void setup()
 {
  
-  Serial.begin(115200);
-  lcd.init();
-  // turn on LCD backlight                      
-  lcd.backlight();
+  Serial.begin(9600);
+  Serial.print("hello!");
+  tft.begin();
+
+  Serial.println("init");
+
+//  tft.fillRect(0, 0, 128, 128, BLUE);
+//  delay(2000);
+  tft.fillScreen(BLACK);
   
   emon1.voltage(35, 220, 1.7);  // Voltage: input pin, calibration, phase_shift
   emon1.current(34, 0.5);   
@@ -79,7 +109,9 @@ void setup()
   Serial.println("Connected...");
   
 }
- 
+
+
+
 void loop()
 {
   
@@ -87,92 +119,116 @@ emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossin
   kWh = kWh + emon1.apparentPower * (millis() - lastmillis) / 3600000000.0;
            
 
-  float realPower       = emon1.realPower;  //extract Real Power into variable
+  realPower       = emon1.realPower;  //extract Real Power into variable
   Serial.print("\tReal Power: ");
-  Serial.println(realPower);
-  lcd.setCursor(0, 0);
-  lcd.print("R.Power:");
-  lcd.print(realPower);
-  lcd.print("W");
-  delay(100); 
-  json.set("/realPower", realPower);
+  Serial.println(realPower); 
+  String realpower = String(realPower,2);
+  json.set("/Real Power", realpower);
   Firebase.updateNode(firebaseData,"/",json);
 
 
   
-  float apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
+  apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
   Serial.print("\tApparent Power: ");
-  Serial.println(apparentPower);
-  lcd.setCursor(0, 1);
-  lcd.print("A.Power:");
-  lcd.print(apparentPower);
-  lcd.print("W");
-  delay(100); 
-  json.set("/apparentPower", apparentPower);
-  delay(2000);
+  Serial.println(apparentPower); 
+  String apparentpower = String(apparentPower,2);
+  json.set("/Apparent Power", apparentpower);
+  
+  
+  powerFActor = emon1.powerFactor;      //extract Power Factor into Variable
+  Serial.print("\tPower Factor: ");
+  Serial.println(powerFActor);
+  String powerfactor = String(powerFActor,2);
+  json.set("/Power Factor", powerfactor);
+ 
+
+  supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
+  Serial.print("\tVoltage: ");
+  Serial.println(supplyVoltage); 
+  String supplyvoltage = String(supplyVoltage,2);
+  json.set("/Voltage", supplyvoltage);
 
 
   
-   
-  lcd.clear();
-  float powerFActor = emon1.powerFactor;      //extract Power Factor into Variable
-  Serial.print("\tPower Factor: ");
-  Serial.println(powerFActor);
-  lcd.setCursor(0, 0);
-  lcd.print("P.Factor:");
-  lcd.print(powerFActor);
-  lcd.print("W");
-  delay(100); 
-  json.set("/powerFActor", powerFActor);
- 
-
-
-   
-  float supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
-  Serial.print("\tVoltage: ");
-  Serial.println(supplyVoltage); 
-  lcd.setCursor(0, 1);
-  lcd.print("Voltage:");
-  lcd.print(supplyVoltage);
-  lcd.print("V");
-  delay(100); 
-  json.set("/supplyVoltage", supplyVoltage);
- 
-  delay(3000);
-
-
-
-
-
-
-  lcd.clear();
-  float Irms = emon1.Irms;             //extract Irms into Variable
+  Irms = emon1.Irms;             //extract Irms into Variable
   Serial.print("\tIrms: ");
   Serial.println(Irms);
-  lcd.setCursor(0, 0);
-  lcd.print("Irms:");
-  lcd.print(Irms);
-  lcd.print("A");  
-  delay(100); 
-  json.set("/Irms", Irms);
-
-
+  String IRMS = String(Irms,2);
+  json.set("/Current", IRMS);
 
   
   Serial.print("\tKwh: ");
   Serial.println(kWh);
   lastmillis = millis();
-  lcd.setCursor(0, 1);
-  lcd.print("Kwh:");
-  lcd.print(kWh);
-  lcd.print("W");
-  delay(100); 
-  json.set("/KWh", kWh);
+  String kwh = String(kWh,2);
+  json.set("/KWh", kwh);
 
-  delay(1000);
+    tft.setTextColor(WHITE);
+    tft.setCursor(13,65);
+    tft.print("RP");
+    tft.setCursor(60,65);
+    tft.print(realPower);
+    tft.drawRoundRect(0, 62, 125, 20, 5,WHITE);
+    tft.drawRoundRect(1, 63, 123, 18, 5,CYAN);
+    tft.drawLine(45, 63, 45, 89, CYAN);
+    tft.drawLine(46, 63, 46, 89, CYAN);
 
+    tft.setTextColor(CYAN);
+    tft.setCursor(13,85);
+    tft.print("AP"); 
+    tft.setCursor(60,85);
+    tft.print(apparentPower);
+    tft.drawRoundRect(0, 82, 125, 20, 5,WHITE);
+    tft.drawRoundRect(1, 83, 123, 18, 5,CYAN);
+    tft.drawLine(45, 83, 45, 109, CYAN);
+    tft.drawLine(46, 83, 46, 109, CYAN);
+
+    tft.setTextColor(WHITE);
+    tft.setCursor(7,105);
+    tft.print("P.F");
+    tft.setCursor(60,105);
+    tft.print(powerFActor);
+    tft.drawRoundRect(0, 102, 125, 20, 5,WHITE);
+    tft.drawRoundRect(1, 103, 123, 18, 5,CYAN);
+    tft.drawLine(45, 103, 45, 120, CYAN);
+    tft.drawLine(46, 103, 46, 120, CYAN);
+
+    tft.setTextColor(CYAN);
+    tft.setTextSize(2);
+    tft.setCursor(18,5);
+    tft.print("V");
+    tft.setCursor(60,5);
+    tft.print(supplyVoltage);
+    tft.drawRoundRect(0, 2, 125, 20, 5,WHITE);
+    tft.drawRoundRect(1, 3, 123, 18, 5,CYAN);
+    tft.drawLine(45, 3, 45, 20, CYAN);
+    tft.drawLine(46, 3, 46, 20, CYAN);
+
+    tft.setTextColor(WHITE);
+    tft.setCursor(18,25);
+    tft.print("I");
+    tft.setCursor(60,25);
+    tft.print(Irms);
+    tft.drawRoundRect(0, 22, 125, 20, 5,WHITE);
+    tft.drawRoundRect(1, 23, 123, 18, 5,CYAN);
+    tft.drawLine(45, 23, 45, 39, CYAN);
+    tft.drawLine(46, 23, 46, 39, CYAN);
+
+
+    tft.setTextColor(CYAN);
+    tft.setCursor(7,45);
+    tft.print("KWh");
+    tft.setCursor(60,45);
+    tft.print(KWh);    
+    tft.drawRoundRect(0, 42, 125, 20, 5,WHITE);
+    tft.drawRoundRect(1, 43, 123, 18, 5,CYAN);
+    tft.drawLine(45, 43, 45, 65, CYAN);
+    tft.drawLine(46, 43, 46, 65, CYAN);
+  delay(3000);
  
+ tft.fillScreen(BLACK);
 }
 
-   
+
+
 
